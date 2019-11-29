543a381d387eaacb3ba4f0ec8cbaa773f76a761e
vinod <chandravinod.s@cisinlabs.com>
2019-11-29 16:13:48 +0530

    Webhook file updated

--- includes/webhook.php
+++ includes/webhook.php
@@ -1,6 +1,5 @@
 <?php
 	$responseData = file_get_contents("php://input");
-	$log_filename = 'request.log';
-	$responseData = 'New Webhook log ::- '.date('Y-m-d H:i:s').' array : ' .$responseData; 	
-	file_put_contents($log_filename, $responseData, FILE_APPEND);
+	$log_filename = 'request.log';	
+	file_put_contents($log_filename, $responseData);
 ?>
