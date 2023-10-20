e03a5a502034bad20755b27dc9e6a10f432e50e3
Martin Boer <martin@myparcel.com>
2023-10-20 11:27:34 +0200

    :pushpin: tag version 2.1.10

--- includes/common/myparcel-constant.php
+++ includes/common/myparcel-constant.php
@@ -1,6 +1,6 @@
 <?php
 
-define('MYPARCEL_PLUGIN_VERSION', '2.1.9');
+define('MYPARCEL_PLUGIN_VERSION', '2.1.10');
 define('SHIPMENT_PLUGIN_NOTICE', 'alive');
 define('GET_META_MYPARCEL_SHIPMENT_KEY', 'myparcel_shipment_key');
 define('GET_META_SHIPMENT_TRACKING_KEY', 'shipment_track_key');
