aa47a979411be5834f0b3cb7aecb4dfe3d6bbf9c
Martin Boer <martin@myparcel.com>
2024-04-18 18:41:41 +0200

    :pushpin: tag version 2.1.14

--- woocommerce-connect-myparcel.php
+++ woocommerce-connect-myparcel.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
  * Plugin Name: MyParcel.com
  * Plugin URI: https://help.myparcel.com/home/integrations#Integrations-WooCommerce
  * Description: This plugin enables you to export WooCommerce orders to MyParcel.com.
- * Version: 2.1.13
+ * Version: 2.1.14
  * Author: MyParcel.com
  * Author URI: https://www.myparcel.com
  * Requires at least:
