9b067a17e4de8a188f1daf4c69f11a078cfe26dd
Martin Boer <martin@myparcel.com>
2024-07-03 14:22:09 +0200

    :technologist: replace custom woocommerce check with plugin requirement

--- woocommerce-connect-myparcel.php
+++ woocommerce-connect-myparcel.php
@@ -12,6 +12,7 @@ declare(strict_types=1);
  * Requires at least:
  * Tested up to:
  * Requires PHP: 8.0
+ * Requires Plugins: woocommerce
  *
  * @package WooCommerceConnectMyParcel
  */
@@ -20,21 +21,10 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
 
-$wooVersionMessage = '<p>Please install <a href="https://wordpress.org/plugins/woocommerce/" target="_blank">WooCommerce</a> to use the MyParcel.com plugin.</p>';
-
-if (!in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
-    global $pagenow;
-    if ('plugins.php' === $pagenow) {
-        add_action('admin_notices', function () use ($wooVersionMessage) {
-            echo '<div class="notice notice-error is-dismissible">' . $wooVersionMessage . '</div>';
-        });
-    }
-} else {
-    require_once dirname(__FILE__) . '/vendor/autoload.php';
-    require_once dirname(__FILE__) . '/includes/myparcel-api.php';
-    require_once dirname(__FILE__) . '/includes/common/myparcel-constant.php';
-    require_once dirname(__FILE__) . '/includes/common/common-functions.php';
-    require_once dirname(__FILE__) . '/includes/myparcel-hooks.php';
-    require_once dirname(__FILE__) . '/includes/myparcel-shipment-hooks.php';
-    require_once dirname(__FILE__) . '/includes/myparcel-settings.php';
-}
+require_once dirname(__FILE__) . '/vendor/autoload.php';
+require_once dirname(__FILE__) . '/includes/myparcel-api.php';
+require_once dirname(__FILE__) . '/includes/common/myparcel-constant.php';
+require_once dirname(__FILE__) . '/includes/common/common-functions.php';
+require_once dirname(__FILE__) . '/includes/myparcel-hooks.php';
+require_once dirname(__FILE__) . '/includes/myparcel-shipment-hooks.php';
+require_once dirname(__FILE__) . '/includes/myparcel-settings.php';
